55bd2652ec1c389ea86078f9348255431ef02e27
Ashutosh Sharma <ashlightyear@gmail.com>
2024-03-26 16:25:52 +0530

    feat: Added navbar component in React app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
+function Navbar(){
+    return (
+        <>
+            <h1>Navbar in React</h1>
+            <ul>
+                <li>User</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar;
